1cef1591908f28b3231707f3dcd84740cc1c286d
montoya <montoyaguzman7@gmail.com>
2017-12-18 17:33:37 -0600

    funcionando primera valiadacion de grupo

--- src/app/components/template/data/data.component.ts
+++ src/app/components/template/data/data.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
-  styles: []
+  styles: [],
+  styleUrls: ['./data.component.css']
 })
 export class DataComponent implements OnInit {
   titleData = 'Formulario Data';
+  formulario: FormGroup;
 
-  constructor() { }
-
+  constructor() {
+    this.formulario = new FormGroup({
+      'nombre': new FormControl('',[
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      'apellido': new FormControl('',[
+        Validators.required
+      ]),
+      'correo': new FormControl('',[
+        Validators.required,
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}")
+      ]),
+    });
+  }
 
   ngOnInit() {
+  }
+
+  registrar() {
+    console.log("Formulario", this.formulario.value);
   }
 
 }
