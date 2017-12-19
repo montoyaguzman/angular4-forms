21036a142f4ca80772edad4d64afa701f1031d21
montoya <montoyaguzman7@gmail.com>
2017-12-19 00:29:58 -0600

    uso de agrupaciones en formulario data

--- src/app/components/template/data/data.component.ts
+++ src/app/components/template/data/data.component.ts
@@ -11,23 +11,35 @@ export class DataComponent implements OnInit {
   titleData = 'Formulario Data';
   formulario: FormGroup;
 
+  usuario: Object = {
+    nombreCompleto: {
+      nombre: "andy",
+      apellido: "leon"
+    },
+    correo: "andrea@hotmail.com"
+  };
+
   constructor() {
+    console.log(this.usuario);
+
     this.formulario = new FormGroup({
-      'nombre': new FormControl('',[
-        Validators.required,
-        Validators.minLength(5)
-      ]),
-      'apellido': new FormControl('',[
-        Validators.required
-      ]),
-      'correo': new FormControl('',[
+      'nombreCompleto' : new FormGroup({
+        'nombre': new FormControl('', [
+          Validators.required,
+          Validators.minLength(5)
+        ]),
+        'apellido': new FormControl('', Validators.required)
+      }),
+      'correo': new FormControl('', [
         Validators.required,
         Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}")
       ]),
     });
+    
   }
 
   ngOnInit() {
+
   }
 
   registrar() {
