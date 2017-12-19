01660b215605ae8ffe179bb08d03a7a28f5359c4
montoya <montoyaguzman7@gmail.com>
2017-12-19 01:46:10 -0600

    practicas necesarias terminadas

--- src/app/components/template/data/data.component.ts
+++ src/app/components/template/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -11,12 +11,13 @@ export class DataComponent implements OnInit {
   titleData = 'Formulario Data';
   formulario: FormGroup;
 
-  usuario: Object = {
+  usuario: any = {
     nombreCompleto: {
       nombre: "andy",
       apellido: "leon"
     },
     correo: "andrea@hotmail.com"
+    // pasatiempos: ["Ver series", "Leer", "Correr"]
   };
 
   constructor() {
@@ -24,18 +25,27 @@ export class DataComponent implements OnInit {
 
     this.formulario = new FormGroup({
       'nombreCompleto' : new FormGroup({
+        // 'nombre': new FormControl(this.usuario.nombreCompleto.nombre, [
         'nombre': new FormControl('', [
-          Validators.required,
-          Validators.minLength(5)
-        ]),
-        'apellido': new FormControl('', Validators.required)
+                                        Validators.required,
+                                        Validators.minLength(5)
+                                      ]),
+        'apellido': new FormControl('', [
+                                          Validators.required,
+                                          this.apellidoProhibido
+                                        ])
       }),
       'correo': new FormControl('', [
-        Validators.required,
-        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}")
-      ]),
+                                      Validators.required,
+                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}")
+                                    ]),
+      // 'pasatiempos': new FormArray([])
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl()
     });
-    
+
+    // this.formulario.setValue(this.usuario);
+
   }
 
   ngOnInit() {
@@ -44,6 +54,36 @@ export class DataComponent implements OnInit {
 
   registrar() {
     console.log("Formulario", this.formulario.value);
+
+    this.formulario.reset({
+      nombreCompleto: {
+        nombre: "",
+        apellido: ""
+      },
+      correo: ""
+    });
+    // this.formulario.controls['correo'].setValue("nuevocorreo@servidor.com")
+
+    this.formulario.controls['password2'].setValidators([
+      Validators.required,
+      this.passwordDiferente
+    ])
+
   }
+
+  apellidoProhibido(control: FormControl): {[s: string]:boolean} {
+    if (control.value === "bobo") {
+      return {apellidoProhibido: true}
+    }
+    return null;
+  }
+
+  passwordDiferente(control: FormControl): {[s: string]:boolean} {
+    if (control.value !== this.formulario.controls['password1'].value) {
+      return {passwordDiferente: true}
+    }
+    return null;
+  }
+
 
 }
